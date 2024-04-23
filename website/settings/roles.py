13a4f9714f76f62f7ada7fb568a5eccf9abf8539
Anthony <nguyenanthony021029@gmail.com>
2024-04-23 08:24:24 -0700

    last

--- website/settings/roles.py
+++ website/settings/roles.py
@@ -8,18 +8,18 @@ from werkzeug.security import generate_password_hash, check_password_hash
 #Delete user:
 class Delete_user(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    admin_username = db.Column(db.String(100), nullable=False)
-    deleted_user = db.Column(db.String(100), nullable=False)
-    delete_time = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    admin_username = db.Column(db.String(20), nullable=False)
     admin_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    deleted_user = db.Column(db.String(20), nullable=False)
+    delete_time = db.Column(db.DateTime, nullable=False, default=datetime.now)
     admin = db.relationship('User', foreign_keys=[admin_id], backref='deleted_users', uselist=False)
 
 #User information
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), nullable=False, unique=True)
-    password = db.Column(db.String(100), nullable=False)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.String(20), nullable=False)
     role = db.Column(db.String(20), nullable=False)
     deleted = db.relationship('Delete_user', backref='admin_user', foreign_keys='Delete_user.admin_id')
     def check_password(self, password):
@@ -30,15 +30,15 @@ class User(db.Model, UserMixin):
 #Book Checkout Information
 class checkout_books_role(db.Model):
     id= db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100),nullable=True)
-    bookname = db.Column(db.String(100), nullable=True)
+    username = db.Column(db.String(20),nullable=True)
+    bookname = db.Column(db.String(20), nullable=True)
     returndate = db.Column(db.DateTime, nullable=False)
 
 #Database information
 class Book_data(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    book_title = db.Column(db.String(100),nullable=False)
-    book_genre = db.Column(db.String(100), nullable=False)
+    book_title = db.Column(db.String(20),nullable=False)
+    book_genre = db.Column(db.String(20), nullable=False)
     
 @login.user_loader
 def load_user(id):
