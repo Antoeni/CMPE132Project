1f251770d9267a6505709a4086cb17058912ee36
Antoeni <nguyenanthony021029@gmail.com>
2024-04-23 20:42:07 -0700

    last push and update

--- website/__init__.py
+++ website/__init__.py
@@ -5,21 +5,19 @@ from flask_login import LoginManager
 
 
 app = Flask(__name__)
-basedir = os.path.abspath(os.path.dirname(__file__))
+#This sets the file path
+pathTodb = os.path.abspath(os.path.dirname(__file__))
 
 app.config.from_mapping(
     SECRET_KEY = 'secret password',
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'website.db'),
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI='sqlite:///' + os.path.join(pathTodb, 'website.db'),
 )
 
 #This will create the data base and login, for when we call routes
 db = SQLAlchemy(app)
 login = LoginManager(app)
 
-
 login.login_view = 'login'
-
 from website import routes
 
 
